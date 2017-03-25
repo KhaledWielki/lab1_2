e5950c0d6a7b450700fec1cb62b4322e2c644dc8
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 18:27:25 +0100

    Payment clas refactor

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -18,20 +18,26 @@ package pl.com.bottega.ecommerce.sales.domain.payment;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
-
+ 
 public class Payment {
-
-	private ClientData clientData;
-
-	private Money amount;
-
-	private Id aggregateId;
-
-	private Payment payment;
-
-	public Payment rollBack() {
-		Id id = Id.generate();
-		
-		return payment;
+ 
+ 	private ClientData clientData;
+ 
+ 	private Money amount;
+ 
+	private PaymentFactory paymentFactory;
+ 
+ 	private Id aggregateId;
+ 
+ 
+	Payment(Id aggregateId, ClientData clientData, Money amount) {
+ 		this.aggregateId = aggregateId;
+ 		this.clientData = clientData;
+ 		this.amount = amount;
+ 	}
+ 
+ 	public Payment rollBack() {
+ 
+ 		return paymentFactory.createPayment(clientData, amount.multiplyBy(-1));
 	}
-}
+ } 
